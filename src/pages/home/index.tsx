d07c35b2fae2c412bd86475ebe95a636e1b069b3
lucas <luquinha_0912@hotmail.com>
2023-05-27 23:02:40 -0300

    servicos

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -21,6 +21,33 @@ export default function Home() {
         </nav>
       </header>
       <section className="section-1"></section>
+      <section className="section-2">
+        <div className="title">
+          <p>
+            Nossos Serviços
+          </p>
+        </div>
+        <div className="container-services">
+          <div className="logo-banho">
+            <p>
+              Banho & Tosa 
+            </p>
+            <div className="banho-description">
+            </div>
+          </div>
+          <div className="logo-spa">
+            <p>
+              Spa
+            </p>
+          </div>
+          <div className="logo-veterinario">
+            <p>
+              Serviços Veterinários
+            </p>
+          </div>
+        </div>
+      </section>
+
     </div>
   );
 }
